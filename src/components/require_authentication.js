1ada74fddde5212598f332dc6507dbb2605fe06b
jackbaber <thejackbabs@gmail.com>
2018-03-26 11:18:59 +0100

    redirects user on basis of authenticated status

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,8 +3,18 @@ import { connect } from 'react-redux'
 
 export default function(ComposedComponent){
   class Authentication extends Component{
+    componentWillMount(){
+      if(!this.props.authenticated){
+        this.props.history.push('/')
+      }
+    }
+
+    componentWillUpdate(nextProps){
+      if(!nextProps.authenticated){
+        this.props.history.push('/')
+      }
+    }
     render(){
-      console.log(this.props.authenticated)
       return <ComposedComponent {...this.props} />
     }
   }
